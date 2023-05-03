a620cae31aa46cda2864a5ee73ac5efb527a10f9
szymonberdzik <szymon.berdzik@softnauts.com>
2023-05-03 14:18:33 +0200

    feat: add subprocess validation and optimise replace_activities_with_subprocess method

--- index.py
+++ index.py
@@ -9,11 +9,19 @@ if __name__ == "__main__":
         logService = LogService(log)
 
         logService.find_subprocess()
-        new = logService.replace_activities_with_subprocess()
+        print(log.subprocesses)
+        new = logService.replace_activities_with_subprocess() 
+
+
+        # print(log.process_tree)
+        # print(log.subprocesses)
         # print(logService.find_subprocess())
-        # logService.view_whole_log_process()
-        logService.view_process_tree(new)
+
+        # logService.view_whole_log_process_petri()
+        # logService.view_process_tree(log)
+        # logService.view_process_tree(new)
         logService.view_process_bmpn(new)
+        logService.view_process_bmpn(log)
 
     except Exception as e:
         print(e)
--- logs/LogService.py
+++ logs/LogService.py
@@ -4,9 +4,11 @@ from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
 from pm4py.visualization.dfg import visualizer as dfg_visualization
 import pm4py
 from logs import Log, LogFactory
-import xml.etree.ElementTree as ET
-import os
-
+import pandas as pd
+import time
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 class LogService:
 
@@ -21,9 +23,9 @@ class LogService:
         # 4-7 eventow i nie wszystkie chcemy grupowac 
         question = "If I have a process with the following tree: " + str(
             self._log.process_tree) + "Can you suggest how the subprocess could be grouped based on the provided activities. In one subpocess there should be from 4 to 7 activities no more no less. Group only activities that are matching, not every sent activity has to relate to subprocess." \
-                               "I " \
-                               "would really like the result to be only in json format, without unnecessary text, " \
-                               "please [{name: \"Name of subprocess\", \"actions\": []},...]? "
+                            "I " \
+                            "would really like the result to be only in json format, without unnecessary text, " \
+                            "please [{name: \"Name of subprocess\", \"actions\": []},...]? "
         string = self._chat.askGPT(question)
         outputAsDict = JsonService.transform_gpt3_output(string)
 
@@ -31,7 +33,7 @@ class LogService:
         return outputAsDict
 
     def view_whole_log_process_petri(self) -> None:
-        self._view_log(self._log)
+        self._view_process_petri(self._log)
 
     def view_transformed_log_process_petri(self) -> None:
         log = self.replace_activities_with_subprocess()
@@ -51,6 +53,28 @@ class LogService:
         dfg_visualization.view(gviz)
 
     def replace_activities_with_subprocess(self) -> Log:
+        logging.info("started: replace_activities_with_subprocess")
+        start = time.time()
+
+        self.subprocesses_validation();
+        temp = self._log.source.copy()
+
+        subprocess_map = {}
+        for subprocess_name, activities in self._log.subprocesses.items():
+            subprocess_map.update({activity: subprocess_name for activity in activities})
+
+        temp['concept:name'] = temp['concept:name'].apply(lambda x: subprocess_map.get(x, x))
+
+        end = time.time()
+        logging.info( "finished: replace_activities_with_subprocess in time: %s ", (end - start))
+
+        return LogFactory.create(temp)
+
+    @DeprecationWarning
+    def replace_activities_with_subprocess_old_way(self) -> Log:
+        logging.info("started: replace_activities_with_subprocess_old_way")
+        start = time.time()
+        self.subprocesses_validation();
 
         temp =  self._log.source
         for index, row in self._log.source.iterrows():
@@ -59,11 +83,60 @@ class LogService:
             if subproccess:
                 temp.loc[index, 'concept:name'] = subproccess
 
+        end = time.time()
 
-
+        logging.info( "finished: replace_activities_with_subprocess_old_way in time: %s", (end - start))
         return  LogFactory.create(temp)
 
 
+    def subprocesses_validation(self):
+        subprocess_copy = self._log.subprocesses.copy()
+        for key in subprocess_copy.keys():
+           x = self._validate_subprocess(self._log.subprocesses[key])
+           if x is False:
+                del self._log.subprocesses[key]
+
             
+        
+    def _validate_subprocess(self, subprocess) -> bool:
+        """ subprocess should has atleast 3 activities inside and they should be in relation"""
+
+        if len(subprocess) < 3:
+            return False
+
+        filtered = pm4py.filter_directly_follows_relation(self._log.source, [subprocess])
+        if isinstance(filtered, pd.DataFrame):
+            return True
+        elif len(subprocess) > 2: 
+            subprocess.pop()
+            self._validate_subprocess(subprocess)
+        
+        return False
+
+
+
+    # def search(self,log: ProcessTree, a: list):
+
+    #     # -> - od nastepnego poziomu idziemy od lewej
+    #     #  + oba musza sie wykonac
+    #     #  * to jest jakby petla ktora pozwala nam powtorzyc czynnosc z lewej strony?
+    #     # X wybierana jest jedna czynnosc
+    #     # gdy oba sa None no to wtedy po prostu jest przejscie do dalszego kroku 
+
+    #     if log.operator is None and log.label is None:
+    #         return
+
+
+
+    #     print(log.label, log.operator)
+    #     # if log.operator is None and log.label is not None:
+    #     #     subprocess = JsonService.find_key_by_value(self._log.subprocesses, log.label)
+    #     #     if subprocess:
+    #     #         log._set_label(subprocess)
+
+    #     for i in range(0, len(log.children)):
+    #         self.search(log.children[i]);
+    
 
         
+
